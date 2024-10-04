16d7b4858ff8872f277c93185bbf732369eb7289
Rodrigues <dirs26@gmail.com>
2024-10-04 13:39:27 +0200

    adding ft_strlcat and ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -10,3 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+// #include <stdio.h>
+
+char *strnstr (char *big, const char *little, unsigned int len) 
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j] && little[j] && i + j < len)
+			j++;
+		if (!little[j])
+			return ((char *)big + i);
+		i++;
+	}
+	return (0);
+}
+
+// int main(void)
+// {
+// 	char *big = "Hello World!";
+// 	const char *little = "World";
+// 	unsigned int len = 12;
+// 	printf("%s\n", strnstr(big, little, len));
+// }
