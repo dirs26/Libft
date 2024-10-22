e763c7a5277f38897354810fcb61eb90565fee91
Rodrigues <dirs26@gmail.com>
2024-10-23 01:46:21 +0200

    aa

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -17,17 +17,19 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	sz_src;
 
 	i = 0;
+	sz_src = ft_strlen(src);
 	if (size == 0)
-		return(ft_strlen(src));
+		return(sz_src);
 	while (src[i] != '\0' && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (sz_src);
 }
 /*
 int main() {
