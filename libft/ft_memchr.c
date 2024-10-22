b89bde0fccdf218ccab6d6498645813002190778
Rodrigues <dirs26@gmail.com>
2024-10-23 01:57:59 +0200

    aa

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -24,7 +24,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	{
 		if (str[i] == (unsigned char)c)
 		{
-			return ((void *) &str[i]);
+			return ((void *)&str[i]);
+			i++;
 		}
 	}
 	return (NULL);
