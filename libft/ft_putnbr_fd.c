2a25373f808a14079320b202ed9cf12e5348c316
Rodrigues <dirs26@gmail.com>
2024-10-23 21:11:53 +0200

    drr

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -14,6 +14,13 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+		return ;
+	}
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
