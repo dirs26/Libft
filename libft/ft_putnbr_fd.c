6799bc88c11c008255ac91a4628db92c66961198
Rodrigues <dirs26@gmail.com>
2024-10-23 21:05:52 +0200

    agga

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-void	ft_putbnr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 	{
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -13,7 +13,7 @@
 #include "libft.h"
 #include <stdlib.h>
 
-char	*strnstr(char *big, const char *little, size_t len)
+char	*ft_strnstr(char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	x;
