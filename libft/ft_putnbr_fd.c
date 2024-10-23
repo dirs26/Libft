93d95e492971d2ce77ad0290f8315db081a29b30
Rodrigues <dirs26@gmail.com>
2024-10-23 21:07:03 +0200

    ada

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -21,8 +21,8 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n > 9)
 	{
-		ft_putbnr_fd(n / 10, fd);
-		ft_putbnr_fd(n % 10, fd);
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	else
 		ft_putchar_fd(n + '0', fd);
