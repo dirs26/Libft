5457c579147dd60b305f50c45678661008ecd56a
Rodrigues <dirs26@gmail.com>
2024-10-23 20:56:33 +0200

    aa

--- libft/libft.h
+++ libft/libft.h
@@ -26,7 +26,7 @@ char			*ft_itoa(int n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, unsigned int n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memmove(void *dest, const void *src, unsigned int n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
 void			*ft_memset(void *s, int c, size_t len);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
