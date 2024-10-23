ef7b47a0cac28b9b0325ba26f8bd4f7091466e78
Rodrigues <dirs26@gmail.com>
2024-10-23 03:39:21 +0200

    ASA

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -18,11 +18,10 @@ char	*ft_strdup(const char *s)
 	char	*str;
 	size_t	len;
 
-	len = (ft_strlen(s)+1);
+	len = ft_strlen(s)+1;
 	str = (char *)malloc(len);
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, s, len);
+	if (str != NULL)
+		ft_strlcpy(str, s, len);
 	return (str);
 }
 
