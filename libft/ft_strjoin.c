5d685a4e5578fd6622455b040c94ff0a45bedf66
Rodrigues <dirs26@gmail.com>
2024-10-23 17:09:10 +0200

    aa

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -10,3 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*mem;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	mem = (char *)malloc(len + 1);
+	if (mem == NULL)
+		return (NULL);
+	ft_strlcpy(mem, s1, ft_strlen(s1) + 1);
+	ft_strlcat(mem, s2, len + 1);
+	return (mem);
+}
