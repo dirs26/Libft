790d462fd0bd1adbe177228de4b374ffd84cd175
Rodrigues <dirs26@gmail.com>
2024-10-23 21:02:05 +0200

    aa

--- libft/libft.h
+++ libft/libft.h
@@ -30,7 +30,7 @@ void			*ft_memmove(void *dest, const void *src, size_t n);
 void			*ft_memset(void *s, int c, size_t len);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
-void			ft_putbnr_fd(int n, int fd);
+void			ft_putnbr_fd(int n, int fd);
 void			ft_putstr_fd(char *s, int fd);
 char			**ft_split(char const *s, char c);
 char			*ft_strchr(const char *str, int c);
@@ -40,7 +40,8 @@ unsigned int	ft_strlcat(char *dst, const char *src, unsigned int dstsize);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlen(const char *str);
 int				ft_strncmp(const char *str1, const char *str2, unsigned int n);
-char			*strnstr(char *big, const char *little, size_t len);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char			*ft_strnstr(char *big, const char *little, size_t len);
 char			*ft_strrchr(const char *str, int c);
 void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 char			*ft_strtrim(char const *s1, char const *set);
